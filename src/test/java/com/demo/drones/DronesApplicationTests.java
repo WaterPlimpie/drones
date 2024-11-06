29e65ea2db77409a29d2ebfc31df2599dc76ad1f
Randa <waterplimpie@gmail.com>
2024-11-06 16:16:33 +0200

    [DRM-08] Add API tests

--- src/test/java/com/demo/drones/DronesApplicationTests.java
+++ src/test/java/com/demo/drones/DronesApplicationTests.java
@@ -1,13 +1,60 @@
 package com.demo.drones;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DronesApplicationTests {
 
+	@LocalServerPort
+	private int serverPort;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.reset();
+		RestAssured.port = serverPort;
+		RestAssured.basePath = "/api/v1";
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.requestSpecification = (new RequestSpecBuilder()).setContentType(ContentType.JSON).setAccept(ContentType.JSON).build();
+
+	}
+
 	@Test
-	void contextLoads() {
+	void getDronesWorks() {
+		given()
+				.when()
+				.get("/drones/")
+				.then()
+				.statusCode(200).and()
+				.body("size()", is(10));
+	}
+
+	@Test
+	void addingDroneWhenFleetIsFullThrows() {
+		given()
+				.body("""
+						{
+							"serialNumber": "asldkad",
+							"model": "LIGHTWEIGHT",
+							"weightLimit": 10,
+							"batteryCapacity": 100,
+							"state": "IDLE"
+						}
+						""")
+				.when()
+				.post("/drones/")
+				.then()
+				.statusCode(400);
 	}
 
 }
